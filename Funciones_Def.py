e1d6fb29a2e020b69518cc9dce3950975f2c4b90
MiguelTr7 <mi.tropa@duocuc.cl>
2024-06-17 18:03:04 -0400

    h

--- Funciones_Def.py
+++ Funciones_Def.py
@@ -1,23 +1,26 @@
 import csv
 Lista_registro = []
-descu_salud=0.7
-descu_afp=0.12
+   
 def registro():
     nombre = input("Ingrese su nombre: ");
     apellido = input("Ingrese su apellido: ");
     cargo = input("Ingrese su cargo: ");
-    sueldo_bruto = int(input("Ingrese su sueldo bruto: "));
+    sueldo_bruto = float(input("Ingrese su sueldo bruto: "));
     Lista_registro.append({
         'Nombre': nombre,
         'Apellido': apellido,
         'Cargo': cargo,
         'Sueldo Bruto': sueldo_bruto
 });
+    print("Trabajador registrado exitosamente.")
+
 def Boleta():
  with open('nuevo_archivo.csv', 'r', newline='') as archivo_csv:
     lector_csv = csv.reader(archivo_csv)
     for fila in lector_csv:
-        print(fila)
+        print(fila);
+def lista_registro():
+   
 
 
      
