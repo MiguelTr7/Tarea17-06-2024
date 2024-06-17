8b99c8ccb5ba0588ee8811bdb2536c1141e31f4d
Matias Espinoza <mb.espinoza@duocuc.cl>
2024-06-17 11:56:15 -0400

    Principal

---
+++ #Principal.py
@@ -0,0 +1,21 @@
+Cargos = ["CEO" , "Desarollador" , "ANALista de datos"]
+while True:
+    print ("1.- Registrar trabajador")
+    print ("2.- Listar todos los trabajdores")
+    print ("3.- Imprimir planillas del sueldo")
+    print ("4.- Salir del programa")
+    try:
+        opc = input ("-> ")
+    except ValueError:
+        print ("Ingrese un caracter del 1 al 4")
+    else:
+        if opc == "1":
+            print (1)
+        elif opc == "2":
+            print (2)
+        elif opc == "3":
+            print (3)
+        elif opc == "4":
+            print (4)
+        else: 
+            print ("Ingrese un caracter del 1 al 4")
