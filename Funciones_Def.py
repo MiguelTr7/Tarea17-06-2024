00bcb56664c5b3827b6d4884a35e653af3943ad0
MiguelTr7 <167789563+MiguelTr7@users.noreply.github.com>
2024-06-17 21:01:51 -0400

    Update Funciones_Def.py

--- Funciones_Def.py
+++ Funciones_Def.py
@@ -1,76 +1,37 @@
-import Funciones_Def as funcion
-import time
-#Creamos un menu de 4 opciones 
-Cargos = ["CEO" , "Desarollador" , "Analista de datos"]
-#Usuarios
-user_1 = "miguel"
-user_2 = "mati"
-#Contraseñas
-pass_1 = "1234"
-pass_2 = "4321"
-
-#Inicio Login
-print ("**** LOGIN ****")
-user = input ("Ingrese nombre de usuario: ").lower();
-passw = input ("Ingrese la contraseña: ").lower();
-if user == user_1 and passw == pass_1 or user == user_2 and passw == pass_2:
- print ("Bienvenido al sistema")
- while True:
-        time.sleep (1)
-        print ("..")
-        time.sleep (1)
-        time.sleep (1)
-        print ("...")
-        time.sleep (1)
-        print ("1.- Registrar trabajador")
-        print ("2.- Listar todos los trabajdores")
-        print ("3.- Imprimir planillas del sueldo")
-        print ("4.- Salir del programa")
-        try:
-            opc = input ("-> ")
-        except ValueError:    #Mensaje de error ,,,
-            print ("Ingrese un caracter del 1 al 4")
-        else:
-            if opc == "1":    #Registro ,,,,
-                funcion.registro();
-                print()
-                time.sleep (2)
-            elif opc == "2":   #Listar trabajadores ,,,
-                print (funcion.Lista_registro)
-            elif opc == "3":   #Imrpimir planillas ,,,
-                funcion.imprimir_planilla
-                try:
-                    imp = input ("1.- Imprimir todas las planillas \n2.- Imprimir una planilla \n3.- Salir \n-> ")
-                except ValueError:
-                    print ("Elija una opcion del 1 al 3")
-                else:
-                    if imp == "1":
-                        print ()
-                    elif imp == "2":
-                        print ()
-                    elif imp == "3":
-                        print
-                    else:
-                        print ("Elija una opcion valida")
-
-            elif opc == "4":   #Salir del programa ,,,
-                print ("Gracias por usar el programa, vuelva pronto.")
-                break;
-            else: 
-                print ("Ingrese un caracter del 1 al 4")
-                time.sleep(2)
-                print ("..")
-else:
-        time.sleep (2)
-        print ("..")
-        time.sleep (2)
-        print ("Ingrese un nombre de usuario valido.")
-        time.sleep (2)
-        print ("..")
-        time.sleep (2)
-        print ("..")
-        time.sleep (2)
-        print ()
-        print ()
+import csv
+Lista_registro = [];
+des_salud=0.07;
+des_afp=0.12;
+def registro():
+    nombre=input("Ingrese su nombre: ");
+    apellido=input("Ingrese su apellido: ");
+    cargo= input("Ingrese su cargo: ");
+    sueldo_bruto =int(input("Ingrese su sueldo bruto: "));
+    desc_salud=sueldo_bruto*des_salud;
+    desc_afp=sueldo_bruto*des_afp;
+    sueldo_liquido=sueldo_bruto-desc_salud-desc_afp;
+    print("Trabajador registrado exitosamente.");
+    Lista_registro.append({
+        'Nombre': nombre,
+        'Apellido': apellido,
+        'Cargo': cargo,
+        'Sueldo Bruto': sueldo_bruto,
+        'Descuento Salud': desc_salud,
+        'Descuento AFP': desc_afp,
+        'Sueldo Líquido': sueldo_liquido
+    });
+print("Trabajador registrado exitosamente.")
+"""""Esto Salie en error
+def imprimir_planilla(trabajadores_filtrados,):
+    with open('planilla_trabajadores', "w",encoding='utf-8') as archivo:
+        for trabajador in trabajadores_filtrados:
+            archivo.write(f"Nombre: {trabajador['Nombre']}\n")
+            archivo.write(f"Cargo: {trabajador['Cargo']}\n")
+            archivo.write(f"Sueldo Bruto: {trabajador['Sueldo Bruto']}\n")
+            archivo.write(f"Desc. Salud: {trabajador['Descuento Salud']}\n")
+            archivo.write(f"Desc. AFP: {trabajador['Descuento AFP']}\n")
+            archivo.write(f"Líquido a pagar: {trabajador['Sueldo Líquido']}\n")
+            archivo.write("==============================\n")
+"""
 
      
